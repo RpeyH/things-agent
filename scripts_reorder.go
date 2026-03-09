8e75c8c71d8cbbf6dedb3093c2639638f569c1f5
alnah <alexis.nahan@gmail.com>
2026-03-09 19:17:50 -0300

    refactor: move reorder scripts to internal things

--- scripts_reorder.go
+++ scripts_reorder.go
@@ -1,51 +1,23 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import thingslib "github.com/alnah/things-agent/internal/things"
 
 func scriptResolveAreaRef(areaName, areaID string) string {
-	areaName = escapeApple(strings.TrimSpace(areaName))
-	areaID = escapeApple(strings.TrimSpace(areaID))
-	if areaID != "" {
-		return fmt.Sprintf(`  try
-    set a to first area whose id is "%s"
-  on error errMsg
-    error errMsg
-  end try
-`, areaID)
-	}
-	return fmt.Sprintf(`  try
-    set a to first area whose name is "%s"
-  on error errMsg
-    error errMsg
-  end try
-`, areaName)
+	return thingslib.ScriptResolveAreaRef(areaName, areaID)
 }
 
 func scriptResolveTaskID(bundleID, taskName string) string {
-	return fmt.Sprintf(`tell application id "%s"
-%s  return id of t
-end tell`, bundleID, scriptResolveTaskRef(taskName, ""))
+	return thingslib.ScriptResolveTaskID(bundleID, taskName)
 }
 
 func scriptResolveProjectID(bundleID, projectName string) string {
-	return fmt.Sprintf(`tell application id "%s"
-%s  return id of p
-end tell`, bundleID, scriptResolveProjectRef(projectName, ""))
+	return thingslib.ScriptResolveProjectID(bundleID, projectName)
 }
 
 func scriptReorderProjectItems(bundleID, projectName, projectID string, ids []string) string {
-	return fmt.Sprintf(`tell application id "%s"
-%s  _private_experimental_ reorder to dos in p with ids "%s"
-  return "ok"
-end tell`, bundleID, scriptResolveProjectRef(projectName, projectID), escapeApple(strings.Join(ids, ",")))
+	return thingslib.ScriptReorderProjectItems(bundleID, projectName, projectID, ids)
 }
 
 func scriptReorderAreaItems(bundleID, areaName, areaID string, ids []string) string {
-	return fmt.Sprintf(`tell application id "%s"
-%s  _private_experimental_ reorder to dos in a with ids "%s"
-  return "ok"
-end tell`, bundleID, scriptResolveAreaRef(areaName, areaID), escapeApple(strings.Join(ids, ",")))
+	return thingslib.ScriptReorderAreaItems(bundleID, areaName, areaID, ids)
 }
